4d69e2194cf1f093dc75fd2401af0f59674b17dd
SpataruLoredana <loredana.spataru@softvision.ro>
2019-09-15 04:47:43 +0300

    Updates

---
+++ precache-manifest.bdb65be0b1e1cb1c69259f5ddade6d1f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a8617744f5b246d8ab7aa608e40617",
+    "url": "/github-repos/index.html"
+  },
+  {
+    "revision": "a9e9a5c2516860601921",
+    "url": "/github-repos/static/js/2.358b1037.chunk.js"
+  },
+  {
+    "revision": "8021cd4febbe869a4543",
+    "url": "/github-repos/static/js/main.f3f267b2.chunk.js"
+  },
+  {
+    "revision": "0da89729e5fc881447a6",
+    "url": "/github-repos/static/js/runtime~main.b4e387a6.js"
+  }
+]);
